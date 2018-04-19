ba2129d64327898fd327cd51ad553d0a18fea7d2
lucianokrebs <lkrebs05@gmail.com>
2018-04-19 19:55:01 -0300

    starting with react router, fetching and posting data to a remote server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
-export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=lucianokrebs';
 
 export function fetchPosts() {
 
-    const requests = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return {
         type: FETCH_POSTS,
-        payload: requests
+        payload: request
     };
 }
